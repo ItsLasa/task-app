4fdba17d574c9b4e6e966c4cc307ebec1f32d67c
ItsLasa <manojlasantha306@gmail.com>
2025-05-24 04:57:01 +0530

    user cache save

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider} from "@auth0/auth0-react";
+
+const domain = "dev-l8x4ja4cen628qno.us.auth0.com";
+const clientId = "RlVbFRgosCkAS4sukPcSJ6VwAWESAdTc";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+    cacheLocation="localstorage"
+  >
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
