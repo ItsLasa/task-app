17a931c73aa545726a83c6c1d8d44f1bbcf2a202
ItsLasa <manojlasantha306@gmail.com>
2025-05-24 04:56:37 +0530

    add profile and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
-//App.js
-
+// App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Todo from './components/Todo';
 import Dashboard from './components/Dashboard';
 import TaskAdd from './components/TaskAdd';
-
+import AuthProfile from './components/AuthProfile';
 
 function App() {
   const headStyle = {
     textAlign: "center",
-  }
+  };
+
   return (
     <div>
-      <h1 className='pt-4' style={headStyle}>Task Mangement App</h1>
+      <h1 className='pt-4' style={headStyle}>Task Management App</h1>
+      <AuthProfile />
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Dashboard/>}></Route>
-          <Route path='/view' element={<Todo/>}></Route>
-         
-          <Route path='/add' element={<TaskAdd/>}></Route>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/view' element={<Todo />} />
+          <Route path='/add' element={<TaskAdd />} />
         </Routes>
       </BrowserRouter>
     </div>
